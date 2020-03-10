480736dd7e71d2af2aece32a710f90b50d6bc2f9
vincentlieu <v.lieu93@gmail.com>
2020-03-10 17:03:51 +1100

    Added BMI categories

--- src/bmiCalculator.rb
+++ src/bmiCalculator.rb
@@ -12,18 +12,30 @@
 # Colour code the categories (Yellow, Green, Orange, Red)
 
 require "tty-prompt"
+require "colorize"
 
 def prompt()
     prompt = TTY::Prompt.new
 end
 
-def welcome_msg()
-    welcoming_msg = ["Hi there!", 
-    "This is the Body Mass Index (BMI) calculator.",
+weight_description = {
+    "Underweight" => "you should eat more",
+    "Healthy" => "keep it up!",
+    "Overweight" => "be cautious!",
+    "Obese Class I" => "get off the couch!",
+    "Obese Class II" => "seriously get off the couch!",
+    "Obese Class III" => "seek a medical professional!"
+    }
+
+
+    def welcome_msg()
+    welcoming_msg = [
+    "Body Mass Index (BMI) calculator".colorize(:red),
     "", 
-    "Description:",
+    "Description:".colorize(:red),
     "BMI is a measurement of body fat based on height and weight that applies to men and women aged 18 and above.",
     "It is a screening tool used to indiciate whether an individual is under weight, heathly, over weight or obese.",
+    ""
     ].join("\n") + "\n"
     return welcoming_msg
 end
@@ -35,11 +47,11 @@ end
 
 def bmi_calculation(weight, height, selected_conversion)
     if selected_conversion == "Imperial"
-        bmi_result = (703 * weight) / (height**2)
-        return bmi_result.to_s
+        bmi_result = (703 * weight) / ((height*12)**2)
+        return bmi_result.round(2)
     else
         bmi_result = (weight / (height**2))
-        return bmi_result.to_s
+        return bmi_result.round(2)
     end
 end
 
@@ -48,21 +60,63 @@ def select_conversion_type()
     return conversion_type
 end
 
+def weight_category(bmi_result)
+    if bmi_result < 18.5
+        return category = "Underweight"
+    elsif bmi_result >= 18.5 && bmi_result < 25
+        return category = "Healthy"
+    elsif bmi_result >= 25 && bmi_result < 30
+        return category = "Overweight"  
+    elsif bmi_result >= 30 && bmi_result < 35
+        return category = "Obese Class I"  
+    elsif bmi_result >= 35 && bmi_result < 40
+        return category = "Obese Class II"   
+    else
+        return category = "Obese Class III"    
+    end
+end
+
 puts welcome_msg()
 name = questionaire("What's your name?: ")
 
+play_again = true
+
+while play_again == true
+
 selected_conversion = select_conversion_type()
 
-if selected_conversion == "Imperial"
-    weight = questionaire("What's your weight in pounds?: ").to_f
-    height = questionaire("What's your height in feets?: ").to_f
-else
-    weight = questionaire("What's your weight in kg?: ").to_f
-    height = questionaire("What's your height in metres: ").to_f
-end
+    if selected_conversion == "Imperial"
+        weight = questionaire("What's your weight (pounds): ").to_f
+        height = questionaire("What's your height (feets): ").to_f
+    else
+        weight = questionaire("What's your weight in kg: ").to_f
+        height = questionaire("What's your height in metres: ").to_f
+    end
 
 bmi_result = bmi_calculation(weight, height, selected_conversion)
-puts "Your BMI result is: #{bmi_result}"
+bmi_category = weight_category(bmi_result)
+
+keys = weight_description.keys()
+for key in keys
+    if bmi_category == key
+        advise = weight_description[key]
+    end
+end
+
+
+
+puts "Your BMI result is: #{bmi_result} and you are classed as #{bmi_category}, #{advise}"
+
+calc_again = prompt.yes?('Would you like to re-calculate again?')
+
+if calc_again == true
+    play_again = true
+else
+    play_again = false
+end
+
+end
+
 
 
 
